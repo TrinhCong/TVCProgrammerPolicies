a565d9f9fe1078924a3169d4ab38acafaa88bcc4
Cong Trinh <trinhvan.cong@vietcredit.com.vn>
2022-01-05 21:39:25 +0700

    git: first commit

---
+++ sources/style_dynamic_responsive_callback.php
@@ -0,0 +1,56 @@
+@media only screen and (max-width: 1000px){
+		.header_bottom {
+		background-color: #ffffff;
+	}
+				.header_bottom,
+		nav.mobile_menu{
+			background-color: #ffffff !important;
+		}
+			}
+@media only screen and (min-width: 480px) and (max-width: 768px){
+	
+	        section.parallax_section_holder{
+			height: auto !important;
+			min-height: 400px;
+		}
+		}
+@media only screen and (max-width: 768px){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+@media only screen and (max-width: 600px) {
+			h1, h1 a, body.qode-overridden-elementors-fonts .elementor-widget-heading h1.elementor-heading-title {
+			font-size: 60px; 			line-height: 72px; 		    		}
+	
+			h2, h2 a, body.qode-overridden-elementors-fonts .elementor-widget-heading h2.elementor-heading-title {
+			font-size: 48px; 					    		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+@media only screen and (max-width: 480px){
+
+			section.parallax_section_holder {
+			height: auto !important;
+			min-height: 400px;
+		}
+	
+		
+	}
